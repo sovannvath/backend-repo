acc060f54bfe5a07a79f6ef590e2d761b3136ced
Ouk Sovannvath <155061175+sovannvath@users.noreply.github.com>
2025-06-22 03:45:56 +0000

    yes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    server: {
+        allowedHosts: ["t6dk38-5173.csb.app"], // 👈 Allow the host here
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
